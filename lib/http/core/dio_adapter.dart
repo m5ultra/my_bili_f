1c47d5dccb429b95d73289a367ebe6b65f88c458
Dendi <352671309@qq.com>
2021-11-25 09:39:08 +0800

    完成Dio适配器的编写

---
+++ lib/http/core/dio_adapter.dart
@@ -0,0 +1,45 @@
+import 'package:dio/dio.dart';
+import 'package:my_bili_f/http/core/hi_error.dart';
+import 'package:my_bili_f/http/core/hi_net_adapter.dart';
+import 'package:my_bili_f/http/request/base_request.dart';
+
+/// Dio 适配器
+
+class DioAdapter extends HiNetAdapter {
+  @override
+  Future<HiNetResponse<T>> send<T>(BaseRequest request) async {
+    var response, options = Options(headers: request.header);
+    var error;
+    try {
+      if (request.httpMethod() == HttpMethod.GET) {
+        response = await Dio().get(request.url(), options: options);
+      } else if (request.httpMethod() == HttpMethod.POST) {
+        response = await Dio()
+            .post(request.url(), data: request.params, options: options);
+      } else if (request.httpMethod() == HttpMethod.DELETE) {
+        response = await Dio()
+            .delete(request.url(), data: request.params, options: options);
+      }
+    } on DioError catch (e) {
+      error = e;
+      response = e.response;
+    }
+    if (error != null) {
+      /// 抛出HiNetError
+      throw HiNetError(response?.stateCode ?? -1, error.toString(),
+          data: BuildRes(response, request));
+    }
+    return BuildRes<T>(response, request);
+  }
+
+  /// 构建HiNetResponse
+  HiNetResponse<T> BuildRes<T>(Response response, BaseRequest request) {
+    return HiNetResponse<T>(
+      data: response.data,
+      request: request,
+      statusCode: response.statusCode,
+      statusMessage: response.statusMessage,
+      extra: response,
+    );
+  }
+}
